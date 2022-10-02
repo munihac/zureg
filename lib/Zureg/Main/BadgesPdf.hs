f2eca585167a91f878a2a079742ac43f309c8554
Franz Thoma <franz.thoma@tngtech.com>
2022-10-02 16:54:21 +0200

    Show name & affiliation on card

--- lib/Zureg/Main/BadgesPdf.hs
+++ lib/Zureg/Main/BadgesPdf.hs
@@ -2,14 +2,15 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Zureg.Main.BadgesPdf (main) where
 
-import           Control.Monad        (guard)
+import           Control.Monad        (guard, when)
 import qualified Data.Aeson           as A
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Csv             as Csv
 import           Data.Foldable        (for_)
-import           Data.Maybe           (fromMaybe, mapMaybe, maybeToList)
+import           Data.Maybe           (fromJust, fromMaybe, mapMaybe, maybeToList)
 import qualified Data.Text            as T
 import qualified Graphics.Rendering.Cairo as C
+import qualified Graphics.Rendering.Cairo.SVG as C
 import           System.Environment   (getArgs, getProgName)
 import           System.Exit          (exitFailure)
 import qualified System.IO            as IO
@@ -26,25 +27,25 @@ cardHeight = millimeters 54
 pageWidth = millimeters 210
 pageHeight = millimeters 297
 
-createRegistrantPdf :: [Registrant a] -> C.Render ()
-createRegistrantPdf [] = pure ()
-createRegistrantPdf registrants = do
-    renderPage cardsPerPageH cardsPerPageV registrants
+createRegistrantPdf :: C.SVG -> [Registrant a] -> C.Render ()
+createRegistrantPdf _ [] = pure ()
+createRegistrantPdf template registrants = do
+    renderPage cardsPerPageH cardsPerPageV template registrants
     C.showPage
-    createRegistrantPdf (drop cardsPerPage registrants)
+    createRegistrantPdf template (drop cardsPerPage registrants)
   where
     cardsPerPageH = floor (pageWidth / cardWidth)
     cardsPerPageV = floor (pageHeight / cardHeight)
     cardsPerPage = cardsPerPageH * cardsPerPageV
 
-renderPage :: Int -> Int -> [Registrant a] -> C.Render ()
-renderPage cardsPerPageH cardsPerPageV registrants = do
+renderPage :: Int -> Int -> C.SVG -> [Registrant a] -> C.Render ()
+renderPage cardsPerPageH cardsPerPageV template registrants = do
     C.save
     C.translate leftMargin topMargin
     for_ (distributeOnPage cardsPerPageH cardsPerPageV registrants) $ \row -> do
         C.save
         for_ row $ \reg -> do
-            registrantCard reg
+            registrantCard template reg
             C.translate cardWidth 0
         C.restore
         C.translate 0 cardHeight
@@ -60,15 +61,36 @@ distributeOnPage h v xs =
     let (hd, tl) = splitAt h xs
     in  hd : distributeOnPage h (v-1) tl
 
-registrantCard :: Registrant a -> C.Render ()
-registrantCard Registrant {..} = do
+registrantCard :: C.SVG -> Registrant a -> C.Render ()
+registrantCard template Registrant {..} = do
     C.moveTo 0 0
+    C.svgRender template
     C.rectangle 0 0 cardWidth cardHeight
     C.stroke
-    showText (fromMaybe mempty (riName <$> rInfo))
+    let registrantInfo = fromJust rInfo
+        badgeName = fromMaybe (riName registrantInfo) (riBadgeName registrantInfo)
+        affiliation = riAffiliation registrantInfo
 
-showText :: T.Text -> C.Render ()
-showText = C.showText . T.unpack
+    C.moveTo (cardWidth / 2) 36
+    showTextJustify 24 (0.9 * cardWidth) badgeName
+    case affiliation of
+        Nothing -> pure ()
+        Just a -> do
+            C.moveTo (cardWidth / 2) 72
+            showTextJustify 15 (0.6 * cardWidth) a
+
+showTextJustify :: Double -> Double -> T.Text -> C.Render ()
+showTextJustify fontSize maxWidth txt = do
+    C.save
+    C.setFontSize fontSize
+    C.TextExtents {..} <- C.textExtents txt
+    when (textExtentsWidth > maxWidth) $
+        C.setFontSize (fontSize * maxWidth / textExtentsWidth)
+    C.TextExtents {..} <- C.textExtents txt
+    C.relMoveTo (- textExtentsWidth / 2) 0
+    C.showText txt
+    C.newPath
+    C.restore
 
 main :: forall a. A.FromJSON a => Hackathon a -> IO ()
 main _ = do
@@ -76,17 +98,19 @@ main _ = do
     args     <- getArgs
 
     case args of
-        [exportPath, outputPdf] -> do
+        [exportPath, templateFile, outputPdf] -> do
             registrantsOrError <- A.eitherDecodeFileStrict exportPath
             registrants <- either (fail . show) return registrantsOrError
                 :: IO [Registrant a]
 
+            template <- C.svgNewFromFile templateFile
+
             C.withPDFSurface outputPdf pageWidth pageHeight $ \surface ->
-                C.renderWith surface (createRegistrantPdf registrants)
+                C.renderWith surface (createRegistrantPdf template registrants)
 
         _ -> do
             IO.hPutStr IO.stderr $ unlines
-                [ "Usage: " ++ progName ++ " export.json out.pdf"
+                [ "Usage: " ++ progName ++ " export.json template.svg out.pdf"
                 , ""
                 , "export.json is a list of registrants as obtained by the"
                 , "export tool."
